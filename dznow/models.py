e332e38a15662925ce61434b1188794a2847ab81
AbdelwahabChibah <abdelwahab.chibah@dataimpact.fr>
2019-09-16 23:54:48 +0100

    unified categories

--- dznow/models.py
+++ dznow/models.py
@@ -4,7 +4,7 @@ from rest_framework.exceptions import ValidationError
 
 
 class Category(models.Model):
-    image = models.URLField()
+    image = models.CharField(max_length=1000)
     name = models.CharField(max_length=100, primary_key=True)
 
 
