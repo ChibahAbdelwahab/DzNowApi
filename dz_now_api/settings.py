0867d575d31bfa889af12a15e9543f0480fc4f10
AbdelwahabChibah <abdelwahab.chibah@dataimpact.fr>
2019-09-18 00:19:04 +0100

    pipeline social auth

--- dz_now_api/settings.py
+++ dz_now_api/settings.py
@@ -187,5 +187,17 @@ SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
+SOCIAL_AUTH_PIPELINE = (
+  'social_core.pipeline.social_auth.social_details',
+  'social_core.pipeline.social_auth.social_uid',
+  'social_core.pipeline.social_auth.auth_allowed',
+  'social_core.pipeline.social_auth.social_user',
+  'social_core.pipeline.user.get_username',
+  'social_core.pipeline.social_auth.associate_by_email',
+  'social_core.pipeline.user.create_user',
+  'social_core.pipeline.social_auth.associate_user',
+  'social_core.pipeline.social_auth.load_extra_data',
+  'social_core.pipeline.user.user_details',
+)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
